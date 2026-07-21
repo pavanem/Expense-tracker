e9513b465c534141338d869663c72390f581fb67
pavanHammerer <pavanhammered@gmail.com>
2026-07-21 18:12:51 +0530

    Cleanup

--- backend/src/main/java/com/expensetracker/entity/CategoryStatus.java
+++
@@ -1,9 +0,0 @@
-package com.expensetracker.entity;
-
-/**
- * Represents whether a category is currently selectable for new expenses.
- */
-public enum CategoryStatus {
-    ACTIVE,
-    INACTIVE
-}

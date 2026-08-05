4cf5ef57f3b5d024b38e364e2282d3b3c7202ab6
pavanHammerer <53854028+pavanem@users.noreply.github.com>
2026-08-05 18:52:20 +0530

    fix: acceptance tests

--- acceptance-tests/src/test/java/com/expensetracker/acceptance/CucumberSpringConfiguration.java
+++ acceptance-tests/src/test/java/com/expensetracker/acceptance/CucumberSpringConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 @CucumberContextConfiguration
 @SpringBootTest(
@@ -19,6 +20,7 @@ public class CucumberSpringConfiguration {
     @LocalServerPort
     private int port;
 
+    @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine")
             .withDatabaseName("expense_tracker_test")
